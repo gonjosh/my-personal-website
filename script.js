356d8346757bde890ef6394d250d307095345431
Joshua Gonzalez <gonjosh@gmail.com>
2022-04-29 21:49:23 -0400

    testing git hub page updating

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const h2 = document.createElement("h2");
 
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "This content added by JavaScript, wu tang forever!!!";
+
 
 document.querySelector("body").appendChild(h2);
